60483fb4105ed6743675ee5ebeb956d5815e9388
bramdal <bramhadalvi@gmail.com>
2020-01-18 17:07:35 +0530

    initial commit

---
+++ Assets/PlayerAnimation.cs
@@ -0,0 +1,299 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Animations;
+
+public class PlayerAnimation : MonoBehaviour
+{
+
+    public float tiltRate;
+
+    Animator anim;
+    public PlayerMovement movementScript;
+
+    int strideCount = 2;
+
+    
+    private Vector3 rightFootPosition, leftFootPosition;
+    private Vector3 rightFootIKPosition, leftFootIKPosition;
+    private Quaternion rightFootIKRotation, leftFootIKRotation;
+    private float lastPelvisPositionY, lastRightFootPositionY, lastLeftFootPositionY;
+
+    [Header("Feet Inverse Kinematics Variables")]
+    public bool enableFeetIK = true;
+    public bool enablePelvisShift = false;
+    public bool enableAccelerationTilt = true;
+    [SerializeField] private float heightFromGroundRaycast = 1.5f;
+    [SerializeField] private float raycastDownDistance = 1.5f;
+    [SerializeField] private LayerMask levelLayer;
+    [SerializeField] private float pelvisOffset = 0f;
+    [SerializeField] private float pelvisUpAndDownSpeed = 0.3f;
+    [SerializeField] private float feetToIKPositionSpeed = 0.5f;
+
+    public string rightFootAnimVariableName = "RightFootCurve";
+    public string leftFootAnimVariableName = "LeftFootCurve";
+
+    public bool useFeetRotation;
+    public bool showSolverDebug;
+
+    //head ik
+    public Transform head;
+    Ray headForwardRay;
+    RaycastHit obstacleInfo;
+    Vector3 headSolvedPosition;
+    Vector3 chestSolvedPosition;
+    float slerpTime;
+    bool crouching = false;
+    [Range(0,1)]
+    public float slerpStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void LateUpdate(){
+
+        slerpTime += Time.deltaTime * 5;
+
+        if(movementScript.currentDirection != Vector3.zero){
+            anim.SetBool(("Locomotion"), true);
+            anim.SetFloat("Velocity", movementScript.forwardVelocity);
+        }
+        else{
+            anim.SetBool(("Locomotion"), false);
+            anim.SetFloat("Velocity", 0f);
+        }
+
+        //for state machince
+        // if(movementScript.distanceCovered > movementScript.strideLength /2){
+        //     StepAnimation(movementScript.distanceCovered, movementScript.strideLength);
+        // }
+
+        //for blend tree
+        InterpolateStride(movementScript.distanceCovered, movementScript.strideLength);
+    }
+
+    private void FixedUpdate() {
+        //FeetGrounding
+        if(enableFeetIK == false){return;} //do nothing if feature disabled
+        if(anim == null){return;}   //do nothing if animator not found
+
+        AdjustFeetTarget(ref rightFootPosition, HumanBodyBones.RightFoot); //Adjust feet IK for right foot
+        AdjustFeetTarget(ref leftFootPosition, HumanBodyBones.LeftFoot);  //Adjust feet IK for left foot
+
+        //shoot raycast and solve position of IK
+        SolveFeetPositon(rightFootPosition, ref rightFootIKPosition, ref rightFootIKRotation); 
+        SolveFeetPositon(leftFootPosition, ref leftFootIKPosition, ref leftFootIKRotation); 
+
+        //solve head collission
+        if(GetObstacleInFrontOfHead()){
+            SolveHeadPosition();
+        }
+        
+        if(!crouching){
+            headSolvedPosition = chestSolvedPosition = Vector3.zero;
+        }
+    }
+
+    private void OnAnimatorIK(int layerIndex){
+        if(enableAccelerationTilt && movementScript.currentDirection!= Vector3.zero) 
+            AccelerationTilt(movementScript.currentDirection, movementScript.previousDirection);
+       
+        //feet ik
+        if(enableFeetIK == false){return;}
+        if(anim == null){return;}
+
+        if(enablePelvisShift)
+            MovePelvisHeight(); //tilt and shift pelvis to match legs
+
+        anim.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1f);  //blend weights for IK are 1 by default
+        if(useFeetRotation){
+            anim.SetIKRotationWeight(AvatarIKGoal.RightFoot, anim.GetFloat(rightFootAnimVariableName));
+        }
+
+        MoveFeetToIKPoint(AvatarIKGoal.RightFoot, rightFootIKPosition, rightFootIKRotation, ref lastRightFootPositionY);
+
+        anim.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1f);
+        if(useFeetRotation){
+            anim.SetIKRotationWeight(AvatarIKGoal.LeftFoot, anim.GetFloat(leftFootAnimVariableName));
+        }
+
+        MoveFeetToIKPoint(AvatarIKGoal.LeftFoot, leftFootIKPosition, leftFootIKRotation, ref lastLeftFootPositionY); 
+
+        //head ik
+        MoveHeadToIKPoint();
+        MoveChestToIKPoint();
+    }
+
+    #region accelertion tilt and stride
+    private void AccelerationTilt(Vector3 currentDirection, Vector3 newDirection){
+        if(movementScript.forwardVelocity > 2.5){
+            if(Vector3.Angle(currentDirection, newDirection) != 0f){
+                anim.SetBoneLocalRotation(HumanBodyBones.Hips, Quaternion.Slerp(transform.localRotation, Quaternion.FromToRotation(transform.forward, newDirection) * Quaternion.FromToRotation(transform.forward, transform.up), tiltRate));
+                //note -> quaternion * quaternion is a non commutative
+                //interpolate between current rotation and rotation represented by change in direction after rotating that representation of rotation by 90 degrees to face up (since tilting is changing up direction towards change)
+            } 
+        }
+    }
+
+    private void StepAnimation(float distanceCovered, float strideLength){  
+        movementScript.distanceCovered = 0f;
+        anim.SetTrigger("step");
+        strideCount--;
+        if(strideCount==0){
+            bool currentMirror = anim.GetBool("mirror");
+            anim.SetBool("mirror", !currentMirror);
+            strideCount = 2;
+        }
+    }
+
+    private void InterpolateStride(float distanceCovered, float strideLength){
+        if(movementScript.distanceCovered >= 1f)
+            movementScript.distanceCovered = 0f;
+        anim.SetFloat("DistanceCovered", distanceCovered);
+    }
+    #endregion
+
+    #region FeetIK
+    //FeetGroundingMethods
+
+    void MoveFeetToIKPoint(AvatarIKGoal foot, Vector3 positionIKHolder, Quaternion rotationIKHolder, ref float lastFootPositionY){
+        Vector3 targetIKPosition = anim.GetIKPosition(foot);
+        
+        if(positionIKHolder != Vector3.zero){
+            targetIKPosition = transform.InverseTransformPoint(targetIKPosition);
+            positionIKHolder = transform.InverseTransformPoint(positionIKHolder);
+
+            float lastFootHeight = Mathf.Lerp(lastFootPositionY, positionIKHolder.y, feetToIKPositionSpeed); //store height at which animation places foot bone
+            targetIKPosition.y += lastFootHeight; //adjust for change in height
+
+            lastFootPositionY = lastFootHeight; //feed back temp value after changes
+
+            targetIKPosition = transform.TransformPoint(targetIKPosition); //convert to world space
+            anim.SetIKRotation(foot, rotationIKHolder); //give new foot rotation to anim system
+        }
+        
+        anim.SetIKPosition(foot, targetIKPosition); //give new foot position to anim system
+    }
+
+    private void MovePelvisHeight(){
+        if(rightFootIKPosition == Vector3.zero || leftFootIKPosition == Vector3.zero || lastPelvisPositionY == 0){
+            lastPelvisPositionY = anim.bodyPosition.y;
+            return;
+        }
+
+        float lOffsetPosition = leftFootIKPosition.y - transform.position.y;
+        float rOffsetPosition = rightFootIKPosition.y - transform.position.y;
+
+        float totalOffest = (lOffsetPosition < rOffsetPosition) ? lOffsetPosition : rOffsetPosition;
+
+        Vector3 newPelvisPosition = anim.bodyPosition + Vector3.up * totalOffest;
+
+        newPelvisPosition.y = Mathf.Lerp(lastPelvisPositionY, newPelvisPosition.y, pelvisUpAndDownSpeed);
+
+        anim.bodyPosition = newPelvisPosition;
+        lastPelvisPositionY = anim.bodyPosition.y;
+    }
+
+    //locate feet position using raycasts to solve new position for IK
+    private void SolveFeetPositon(Vector3 fromSkyPosition, ref Vector3 feetIKPosition, ref Quaternion feetIKRotations){
+        RaycastHit feetOutHit;
+
+        if(showSolverDebug)
+            Debug.DrawLine(fromSkyPosition, fromSkyPosition + Vector3.down * (raycastDownDistance + heightFromGroundRaycast), Color.black); 
+
+        if(Physics.Raycast(fromSkyPosition, Vector3.down, out feetOutHit, raycastDownDistance + heightFromGroundRaycast, levelLayer)){
+            feetIKPosition = fromSkyPosition;
+            feetIKPosition.y = feetOutHit.point.y + pelvisOffset;
+            feetIKRotations = Quaternion.FromToRotation(Vector3.up, feetOutHit.normal) * transform.rotation;
+            return;
+        }
+        feetIKPosition = Vector3.zero; // return feet to local origin if no ground found
+    }
+
+    private void AdjustFeetTarget(ref Vector3 feetPositions, HumanBodyBones foot){
+        feetPositions = anim.GetBoneTransform(foot).position;
+        feetPositions.y = transform.position.y + heightFromGroundRaycast;
+    }
+    #endregion
+
+   bool GetObstacleInFrontOfHead(){
+        //headSolvedPosition = Vector3.zero;
+        headForwardRay = new Ray(head.position, transform.forward);
+        if(Physics.SphereCast(headForwardRay, 0.15f, out obstacleInfo, 2f, levelLayer)){
+            if(obstacleInfo.transform.tag == "Obstacle"){
+                return true;
+            }    
+        }
+        return false;
+    }
+
+    void SolveHeadPosition(){
+        chestSolvedPosition = Vector3.zero;
+        slerpTime =slerpStart;
+        int i =0;
+        float heightAtNoObstacle = 0f;
+        while(Physics.SphereCast(headForwardRay, 0.15f, out obstacleInfo, 2f, levelLayer) && obstacleInfo.transform.tag == "Obstacle"){
+            headForwardRay = new Ray(new Vector3(head.position.x, head.position.y - 0.05f, head.position.z), transform.forward);
+            heightAtNoObstacle -= 0.05f;
+            //headSolvedPosition = obstacleInfo.point - head.position;
+            headSolvedPosition = new Vector3(0f, head.position.y - heightAtNoObstacle, 0f);
+            headSolvedPosition.x = headSolvedPosition.z = 0f;
+            //Debug.DrawLine(head.position, head.position - headSolvedPosition, Color.green, 0);
+            //arbitrary counts of recursion
+            i++;
+            if(i>3){ 
+                SolveChestPosition();
+                return;
+            }    
+        }
+    }
+
+    void SolveChestPosition(){
+        headSolvedPosition = Vector3.zero;
+        slerpTime = slerpStart;
+        int i =0;
+        float heightAtNoObstacle = 0f;
+        while(Physics.SphereCast(headForwardRay, 0.15f, out obstacleInfo, 2f, levelLayer) && obstacleInfo.transform.tag == "Obstacle"){
+            headForwardRay = new Ray(new Vector3(head.position.x, head.position.y - 0.05f, head.position.z), transform.forward);
+            heightAtNoObstacle -= 0.05f;
+            //headSolvedPosition = obstacleInfo.point - head.position;
+            chestSolvedPosition = new Vector3(0f, head.position.y - heightAtNoObstacle, 0f);
+            headSolvedPosition.x = headSolvedPosition.z = 0f;
+            //Debug.DrawLine(head.position, head.position - headSolvedPosition, Color.green, 0);
+            //arbitrary counts of recursion
+            i++;
+            if(i>2){ 
+                //chestSolvedPosition = Vector3.zero;
+                return;
+            }    
+        }
+    }
+
+    void MoveHeadToIKPoint(){
+        Quaternion currentRotation = Quaternion.Slerp(anim.GetBoneTransform(HumanBodyBones.Neck).localRotation, Quaternion.FromToRotation(headSolvedPosition, transform.forward), slerpTime);
+        anim.SetBoneLocalRotation(HumanBodyBones.Neck, currentRotation);
+    }
+
+    void MoveChestToIKPoint(){
+        Quaternion currentRotation = Quaternion.Slerp(anim.GetBoneTransform(HumanBodyBones.UpperChest).localRotation, Quaternion.FromToRotation(chestSolvedPosition, transform.forward), slerpTime);
+        anim.SetBoneLocalRotation(HumanBodyBones.UpperChest, currentRotation);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Obstacle"){
+            print("crouch");
+            crouching = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.tag == "Obstacle"){
+            print("get up");
+            crouching = false;
+        }
+    }
+
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //movement variables
+    Vector3 moveDirection;
+    [HideInInspector]public Vector3 currentDirection;
+    [HideInInspector]public Vector3 previousDirection;
+    [HideInInspector]public float forwardVelocity = 0f;
+    Vector3 zDirection;
+    Vector3 xDirection;
+    float verticalVelocity;
+
+    float forwardInput;
+    float rightInput;
+
+    [Header("Movement variables")]
+    public float movementSpeed;
+    public float rotationSpeed;
+    public float gravity;
+    public LayerMask levelMask;
+
+    //references
+    Animator anim;
+    CharacterController charController;
+    Camera cam;
+
+    //layer mask for ground
+    RaycastHit groundHit;
+
+    //force to spring up with when pelvis below optimal standing level
+    public float pelvisSpringForce;
+
+    [Space]
+    [SerializeField]private float walkStrideLength = 0;
+    [SerializeField]private float runStrideLength = 12;
+    public float strideLength;
+    public float distanceCovered;
+
+    [Header("Public references")]
+    //actual pivot of the model
+    public Transform pelvisPosition;
+    Ray pelvisForwardRay;
+    [SerializeField]private float standingPelvisHeightMax = 1.3f; //1.01
+    [SerializeField]private float standingPelvisHeightMin = 1f; //0.95
+    public float currentStandingPelvisHeightMax;
+    public float currentStandingPelvisHeightMin;
+
+    //crouching variables
+    [Space]
+    RaycastHit obstacleInfo;
+
+    //access animation script on the graphic element
+    public PlayerAnimation animationScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //cursor
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = false;
+
+        cam = Camera.main;   
+        anim = GetComponent<Animator>();
+        charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DoLocomotion();
+    }
+
+    // float GetPelvisHeightFromGround(){
+    //     if(Physics.Raycast(pelvisPosition.position, Vector3.down, out groundHit, Mathf.Infinity, levelMask)){
+    //         //Debug.DrawLine(pelvisPosition.position, Vector3.down, Color.green, Time.deltaTime);
+    //         print((groundHit.point - pelvisPosition.position).magnitude);
+    //         return ((groundHit.point - pelvisPosition.position).magnitude);
+    //     }
+    //     return -1;
+    // }
+
+    float GetPelvisHeightFromGround(){
+        if(Physics.Raycast(transform.position, Vector3.down, out groundHit, Mathf.Infinity, levelMask)){
+            //Debug.DrawLine(pelvisPosition.position, Vector3.down, Color.green, Time.deltaTime);
+            print((groundHit.point - transform.position).magnitude);
+            return ((groundHit.point - transform.position).magnitude);
+        }
+        return -1;
+    }
+
+    bool GetObstacleInFront(){
+        pelvisForwardRay = new Ray(pelvisPosition.position, pelvisPosition.forward);
+        if(Physics.SphereCast(pelvisForwardRay, 0.3f, out obstacleInfo, 1f, levelMask)){
+            if(obstacleInfo.transform.tag == "Obstacle"){
+                return true;
+            }    
+        }
+        return false;
+    }
+
+    void SolveHipPosition(){
+        int i =0;
+        float heightAtNoObstacle = 0f;
+        while(Physics.SphereCast(pelvisForwardRay, 0.3f, out obstacleInfo, 1f, levelMask) && obstacleInfo.transform.tag == "Obstacle"){
+            pelvisForwardRay = new Ray(new Vector3(pelvisPosition.position.x, pelvisPosition.position.y - 0.1f, pelvisPosition.position.z), pelvisPosition.forward);
+            heightAtNoObstacle -= 0.1f;
+            currentStandingPelvisHeightMax = pelvisPosition.position.y + heightAtNoObstacle;
+            currentStandingPelvisHeightMin = pelvisPosition.position.y + heightAtNoObstacle;
+            //arbitrary counts of recursion
+            i++;
+            if(i>5){ 
+                return;
+            }    
+        }
+    }
+
+    void DoLocomotion(){
+        previousDirection = new Vector3(moveDirection.x, 0f, moveDirection.z);
+        moveDirection = Vector3.zero;
+
+        rightInput = Input.GetAxis("Vertical");
+        forwardInput = Input.GetAxis("Horizontal");
+
+        zDirection = cam.transform.forward;
+        xDirection = cam.transform.right;
+        zDirection.y = xDirection.y = 0f;
+        zDirection.Normalize();
+        xDirection.Normalize();
+ 
+        currentDirection = moveDirection = xDirection * forwardInput + zDirection * rightInput; 
+        
+        //solver for tilt
+        Debug.DrawLine(transform.position, transform.position + moveDirection, Color.green, 0);
+        Debug.DrawLine(transform.position, transform.position + previousDirection, Color.blue, 0);
+               
+        moveDirection *= movementSpeed;
+
+        forwardVelocity = Vector3.Dot(moveDirection, transform.forward);
+        forwardVelocity = Mathf.Clamp(forwardVelocity, 0.1f, 5f);
+
+        if(moveDirection == Vector3.zero)
+            distanceCovered = moveDirection.magnitude;  
+
+        //stride logic
+        //purely animation based variables, do not use for physics calculations
+        strideLength = Mathf.Lerp(walkStrideLength, runStrideLength, forwardVelocity/ 5f);
+        distanceCovered += moveDirection.magnitude/strideLength * Time.deltaTime;
+        if(distanceCovered > 1f)
+            distanceCovered = 0f;  
+
+        if(moveDirection != Vector3.zero)
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDirection), rotationSpeed);
+
+        //find comfort height for hip to walk underneath
+        if(GetObstacleInFront()){
+            SolveHipPosition();
+        }
+        else{
+            currentStandingPelvisHeightMax = standingPelvisHeightMax;
+            currentStandingPelvisHeightMin = standingPelvisHeightMin;
+        }
+        float pelvisHeightFromGround = GetPelvisHeightFromGround();
+        if(pelvisHeightFromGround > currentStandingPelvisHeightMax){  //ideal value 1.01
+            verticalVelocity -= gravity * Time.deltaTime;
+            verticalVelocity = Mathf.Clamp(verticalVelocity,-Mathf.Infinity, 0f);
+            print("gravity");
+        }
+        else if(pelvisHeightFromGround < currentStandingPelvisHeightMin){  //ideal value 0.95
+            verticalVelocity += pelvisSpringForce * Time.deltaTime; //this implementation does not work for no ground underneath
+            verticalVelocity = Mathf.Clamp(verticalVelocity, 0f, Mathf.Infinity);
+            print("pelvis force");
+        }
+        else 
+        {
+            verticalVelocity = 0f;
+            print("done nothing");
+        }
+        moveDirection.y = verticalVelocity;
+       
+        charController.Move(moveDirection * Time.deltaTime);
+    }
+}
